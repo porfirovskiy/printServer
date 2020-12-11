a4e7a06fdbce872a1b12788d1b1644a25348afd7
porfirovskiy <porfirovskiy@gmail.com>
2020-12-11 11:48:04 +0200

    add composer and public dir

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use PrintServer\RequestHandler;
+
+$n = new RequestHandler();
+$n->test();
+
---
+++ src/RequestHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PrintServer;
+/**
+ * Description of RequestHandler
+ *
+ * @author porfirovskiy
+ */
+class RequestHandler {
+    //put your code here
+    
+    public function test(): void
+    {
+        echo 'dd';
+    }
+    
+}
